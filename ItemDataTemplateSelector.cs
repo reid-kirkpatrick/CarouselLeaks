8e79b9f91f6cbb820dfdaf1d105d11f3fc84262e
Reid Kirkpatrick <reid@multitracks.com>
2025-02-13 10:32:15 -0600

    Added TemplateSelector

---
+++ ItemDataTemplateSelector.cs
@@ -0,0 +1,8 @@
+﻿namespace CarouselLeaks;
+
+internal class ItemDataTemplateSelector : DataTemplateSelector
+{
+	public DataTemplate ItemTemplate { get; set; }
+
+	protected override DataTemplate OnSelectTemplate(object item, BindableObject container) => ItemTemplate;
+}
